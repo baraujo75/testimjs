245f16ca557703bd01efcc6309a0070b0d145416
Bruno Araujo <bruno.bvaraujo@gmail.com>
2021-06-29 18:09:14 -0300

    update

---
+++ js/app.js
@@ -0,0 +1,9 @@
+(function(){
+    const customerImage = document.querySelector('#customer-img')
+    const customerName = document.querySelector('#customer-name')
+    const customerText = document.querySelector('#customer-text')
+    const buttons = document.querySelectorAll('.btn')
+    let index = 0
+    const customers = []
+
+    //Create a new customer using a customer constructor
