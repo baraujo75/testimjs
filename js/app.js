f69ad16627c3436c5c629530704d8ceba67b1413
Bruno Araujo <bruno.bvaraujo@gmail.com>
2021-06-29 18:10:47 -0300

    update

--- js/app.js
+++ js/app.js
@@ -30,3 +30,27 @@
     createCustomer(2, 'Amy', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.')
     createCustomer(3, 'Tyrell', 'If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text.')
     createCustomer(4, 'Wanda', 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.')
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+            if (e.target.parentElement.classList.contains('prevBtn')){
+               if(index === 0){
+                    index = customers.length
+               }
+               index--
+               customerImage.src = customers[index].img
+               customerName.textContent = customers[index].name
+               customerText.textContent = customers[index].text
+            }
+            if (e.target.parentElement.classList.contains('nextBtn')){
+                index++
+                if(index === customers.length){
+                     index = 0
+                }
+                customerImage.src = customers[index].img
+                customerName.textContent = customers[index].name
+                customerText.textContent = customers[index].text
+             }
+        })
+    })
+    
+})()
